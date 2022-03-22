2c5885cb96779b16242e6086015be40ea27deb81
chugogo <ioueasonae86@yahoo.com.tw>
2022-03-22 16:44:50 +0800

    Initial Commit

---
+++ musicPlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicPlay
+//
+//  Created by Chu Go-Go on 2022/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
